04392f8741ffd6993b0f1d75573427f1725e25d2
wenshuojie <1878431458@qq.com>
2022-04-12 10:43:22 +0800

    2022-04-12

---
+++ array/array13.py
@@ -0,0 +1,22 @@
+# 704. 二分查找
+
+from operator import le
+from typing import List
+
+def search(nums: List[int], target: int) -> int:
+    left = 0
+    right = len(nums)-1
+
+    while(left <= right):
+        mid = left + (right-left)//2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        elif nums[mid] > target:
+            right = mid-1
+
+    return -1
+
+print(search([-1,0,3,5,9,12],9))
+print(search([-1,0,3,5,9,12],2))
