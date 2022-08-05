0117f9e4d12a82bf530dad95d692899b8cfa6182
wenshuojie <1878431458@qq.com>
2022-08-05 15:32:33 +0800

    2022-08-05

---
+++ 剑指66/BM17.py
@@ -0,0 +1,19 @@
+# 二分查找-I
+
+from typing import List
+
+class Solution:
+    def search(self , nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+
+        while left <= right:
+            mid = left + (right-left) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                return mid
+        
+        return -1
+
