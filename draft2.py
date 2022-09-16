b81458f0651e4a9ad77c4993a4e017c3bf869529
wenshuojie <1878431458@qq.com>
2022-09-16 21:25:08 +0800

    2022-09-16

--- draft2.py
+++ draft2.py
@@ -1,36 +1,94 @@
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# 冒泡排序
+def bubble(nums):
+    nums_len = len(nums)
+    for i in range(1, nums_len):
+        for j in range(nums_len-i):
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
 
-class Solution:
-    def isSymmetrical(self , pRoot: TreeNode) -> bool:
-        if not pRoot:
-            return True
+# 选择排序
+def select(nums):
+    nums_len = len(nums)
+    for i in range(nums_len-1):
+        min_idx = i
+        for j in range(i+1, nums_len):
+            if nums[j] < nums[min_idx]:
+                min_idx = j
+        
+        nums[min_idx], nums[i] = nums[i], nums[min_idx]
 
-        if self.traverse1(root) == self.traverse2(root):
-            return True
+# 插入排序
+def insert(nums):
+    nums_len = len(nums)
+    for i in range(1, nums_len):
+        temp = nums[i]
+        j = i-1
+        while j >= 0 and nums[j] > temp:
+            nums[j+1] = nums[j]
+            j -= 1
+        nums[j+1] = temp
+
+def shell(nums):
+    pass
+
+# 归并排序
+def mergeSort(nums, left, right):
+    if left >= right:
+        return
+    
+    mid = left + (right-left)//2
+    mergeSort(nums, left, mid)
+    mergeSort(nums, mid+1, right)
+    merge(nums, left, mid, right)
+
+def merge(nums, left, mid, right):
+    nums_len = len(nums)
+    temp = [0 for _ in range(nums_len)]
+
+    for i in range(left, right+1):
+        temp[i] = nums[i]
+    
+    # 合并
+    i, j = left, mid+1
+    for k in range(left, right+1):
+        if j > right or (i <= mid and j <= right and temp[j] > temp[i]):
+            nums[k] = temp[i]
+            i += 1
         else:
-            return False
+            nums[k] = temp[j]
+            j += 1
 
-    def traverse1(self, root, res=[]):
-        if not root:
-            return
-        res.append(root.val)
-        self.traverse1(root.left)
-        self.traverse1(root.right)
-        return res
+# 快速排序
+def quickSort(nums, left, right):
+    if left >= right:
+        return
+    p = partition(nums, left, right)
+    quickSort(nums, left, p-1)
+    quickSort(nums, p+1, right)
 
-    def traverse2(self, root, res=[]):
-        if not root:
-            return
-        res.append(root.val)
-        self.traverse1(root.right)
-        self.traverse1(root.left)
-        return res
+def partition(nums, left, right):
+    p = left
+    i, j = p+1, right
+    while i <= j:
+        while i < right and nums[i] <= nums[p]:
+            i += 1
+        while j > left and nums[j] > nums[p]:
+            j -= 1
+        if i >= j:
+            break
+        nums[i], nums[j] = nums[j], nums[i]
+    nums[p], nums[j] = nums[j], nums[p]
+    return j
 
-from queue import PriorityQueue
-
-a = [1,2]
-print(a.pop())
+if __name__ == '__main__':
+    test_nums = [8,4,8,2,5,0,6]
+    test_nums2 = [1,3,2,4,2]
+    # bubble(test_nums)
+    # select(test_nums)
+    # insert(test_nums)
+    # mergeSort(test_nums2, 0, len(test_nums2)-1)
+    # mergeSort(test_nums, 0, len(test_nums)-1)
+    quickSort(test_nums2, 0, len(test_nums2)-1)
+    quickSort(test_nums, 0, len(test_nums)-1)
+    print(test_nums2)
+    print(test_nums)
